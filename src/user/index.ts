8e5c8cd7053a789fde1404bbb40cab2295d35b63
Duc Nguyen <minhduc140583@gmail.com>
2025-12-28 23:37:12 +0700

    Apply new version

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,25 +1,24 @@
 import { Db } from "mongodb"
-import { buildQuery, Repository, SearchBuilder } from "mongodb-extension"
-import { Log, Search, UseCase } from "onecore"
+import { MongoRepository } from "mongodb-extension"
+import { Log, UseCase } from "onecore"
 import { UserController } from "./controller"
 import { User, UserFilter, userModel, UserRepository, UserService } from "./user"
 export * from "./controller"
 export * from "./user"
 
-export class MongoUserRepository extends Repository<User, string> implements UserRepository {
+export class MongoUserRepository extends MongoRepository<User, string, UserFilter> implements UserRepository {
   constructor(db: Db) {
     super(db, "users", userModel)
   }
 }
 export class UserUseCase extends UseCase<User, string, UserFilter> implements UserService {
-  constructor(search: Search<User, UserFilter>, repository: UserRepository) {
-    super(search, repository)
+  constructor(repository: UserRepository) {
+    super(repository)
   }
 }
 
 export function useUserController(db: Db, log: Log): UserController {
-  const builder = new SearchBuilder<User, UserFilter>(db, "user", buildQuery, userModel)
   const repository = new MongoUserRepository(db)
-  const service = new UserUseCase(builder.search, repository)
+  const service = new UserUseCase(repository)
   return new UserController(service, log)
 }
